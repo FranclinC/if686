bb2036be922ac98c53a78ee9110e70b942ef4fc4
Miguel Araújo <miguelarauj1o@gmail.com>
2015-03-19 08:37:21 -0300

    pasta para estudos

---
+++ estudo/L1_101.hs
@@ -0,0 +1,10 @@
+{- 1) Função 'itemN', que recebe um numero inteiro positivo n e uma lista e retorna o n-ésimo elemento da lista. Ex.: 
+*Main> itemN 2 ["primeiro", "segundo", "terceiro"]
+"segundo" -}
+
+itemN :: int -> [a]
+
+
+main :: IO()
+main = do
+	putStrLn ()
