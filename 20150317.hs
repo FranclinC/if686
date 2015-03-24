9a2de71b023b5ddf42781751a5110f660a050978
Miguel Araújo <miguelarauj1o@gmail.com>
2015-03-24 14:11:52 -0300

    all exercises done! (http://goo.gl/Se44R4)

--- 20150317.hs
+++ 20150317.hs
@@ -1,6 +1,19 @@
-Module Main where
+module Main where
 
 {- Exercícios do slide -}
+
+vendas :: Int -> Int
+vendas x = 1
+
+totalVendas :: Int -> Int
+totalVendas n
+	| (n == 0) = vendas 0
+	| otherwise = totalVendas (n - 1) + vendas n
+
+maxVendas :: Int -> Int
+maxVendas n
+	| (n == 0) = vendas 0
+	| otherwise = maxi (maxVendas (n - 1)) (vendas n)
 
 double :: [Int] -> [Int]
 double [] = []
@@ -28,15 +41,7 @@ quicksort :: [Int] -> [Int]
 quicksort [] = []
 quicksort (x:xs) = quicksort (filter (< x) xs) ++ [x] ++ quicksort (filter (>= x) xs)
 
---generateFiboPairs :: Int -> [Int]
-
---ordDigits :: [Int] -> [Int]
-
-type Ponto = (Float, Float)
-type Reta = (Ponto, Ponto)
-
---pontoY :: Float -> Reta -> Float
-
 main :: IO()
 main = do
 	putStrLn "Yo, bitch!"
+	putStrLn (show (totalVendas 5))
