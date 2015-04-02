6a9ad747c88126ef2540e8d68ed2278634487c9d
Miguel Araújo <miguelarauj1o@gmail.com>
2015-04-02 17:27:14 -0300

    [100%] TRABALHO 02

--- 20150324.hs
+++ 20150324.hs
@@ -2,34 +2,74 @@
 
 -- recebe duas listas ordenadas e retorna uma lista nova
 -- que é o agrupamento das duas, ordenadas
-merge []         ys                   = ys
-merge xs         []                   = xs
-merge xs@(x:xt) ys@(y:yt) | x <= y    = x : merge xt ys
-                          | otherwise = y : merge xs yt
+merge [] ys = ys
+merge xs [] = xs
+merge xs@(x:xt) ys@(y:yt) 
+	| (x <= y) = x : merge xt ys
+	| otherwise = y : merge xs yt
 
 -- decompôe recursivamente a lista
 split :: [Int] -> ([Int], [Int])
 split list
-  | (==) list [] = ([], [])
-  | (==) (length list) 1 = (list, [])
-  | otherwise = (x: xs, y: ys)
-  where
-    (xs, ys) = split (tail (tail list))
-    x = head list
-    y = head (tail list)
+  	| (list == []) = ([], [])
+  	| ((length list) == 1) = (list, [])
+  	| otherwise = (x: xs, y: ys)
+  	where
+		(xs, ys) = split (tail (tail list))
+		x = head list
+		y = head (tail list)
 
--- decompôe, ordena e reagrupa a lista
+-- decompõe, ordena e reagrupa a lista
 mergesort :: [Int] -> [Int]
 mergesort [x] = [x]
-mergesort xs  = let (as, bs) = split xs
-                in merge (mergesort as) (mergesort bs)
+mergesort xs = let (as, bs) = split xs in merge (mergesort as) (mergesort bs)
 
--- Heapsort, mra2
+-- heapsort
+
+add:: Int -> [Int] -> [Int]
+add n hp
+	| hp==[] = [n]
+	| otherwise = (bubbleUp (hp ++ [n]) ((length hp)) 0)
+
+bubbleUp :: [Int] -> Int -> Int -> [Int]
+bubbleUp [] l p  = []
+bubbleUp (h:t) l p 
+	| t == [] || var >= (length t) = (h:t) 
+	| tmp == (l) && (h < (t!!var)) = ([(t!!var)] ++ (take (l-p-1) t)) ++ [h] ++ (drop (l-p) t)
+	| esp == (l) && h < (t!!var) = ([t!!var] ++ (take (l-p-1) t)) ++ [h] ++ (drop (l-p) t)
+	| tmp == (l) || esp == (l) = h:t
+	| (l)>esp = (bubbleUp (bubbleUp (h:(bubbleUp t l (p+1))) tmp p) esp p)
+	| (l)>tmp = (bubbleUp (h:(bubbleUp t l (p+1))) tmp p)
+	| otherwise =  h:(bubbleUp t l (p+1)) 
+		where
+			tmp = ((2*p)+1)
+			var = ((l-p)-1)
+			esp = ((2*p)+2)
 
 
---
+erase :: [Int] -> [Int]
+erase [] = []
+erase [x] = []
+erase [x,y] = [y]
+erase (x:y:z:xs) 
+	| z > y = (add y (z:xs))
+	| otherwise = (add z (y:xs))
+
+sort :: [Int] -> [Int]
+sort [] = []
+sort x = (sort (erase x))  ++  [(head x)]
+		
+buildHeap:: [Int] -> [Int] -> [Int]
+buildHeap [] hp  = hp
+buildHeap (h:t) hp = buildHeap t (add h hp)
+
+heapsort:: [Int] -> [Int]
+heapsort x = sort (buildHeap x [])
 
 main :: IO()
 main = do
-	putStrLn (show [16,4,10,14,7,9,3,2,8])
-	putStrLn (show (heapsort [16,4,10,14,7,9,3,2,8]))
+	putStrLn "Jesse, run!"
+	putStrLn $ show [16,4,10,14,7,9,3,2,8]
+	putStrLn $ show $ mergesort [16,4,10,14,7,9,3,2,8]
+	putStrLn $ show [16,4,10,14,7,9,3,2,8]
+	putStrLn $ show $heapsort [16,4,10,14,7,9,3,2,8]
