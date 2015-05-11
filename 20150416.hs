d43014980c295f96d4bda05ec1bf4dda4096000c
Miguel Araújo <miguelarauj1o@gmail.com>
2015-05-11 17:07:07 -0300

    AULA 06, TRABALHO 08

--- 20150416.hs
+++ 20150416.hs
@@ -1,9 +1,6 @@
-module Main where
+-- AULA 06
 
-import Data.List (sort)
-import Data.Char
-
-{- Oitavo trabalho de PLC -}
+-- TRABALHO 08
 
 aux :: (Ord t, Num t) => [t] -> [t] -> [[t]]
 aux a [] = [a]
@@ -14,18 +11,3 @@ divi x y = [a | a <- (aux y x), a /= []]
 
 listPartitioner :: (Ord t, Num t) => [t] -> ([t] -> [[t]])
 listPartitioner list = \x -> (divi (sort list) (sort x))
-
-{- Exercícios do slide 05_FUNCOES_ALTA_ORDEM -}
-
-posicaoAlfabeto :: String -> [Int]
-posicaoAlfabeto s = map ord s
-
-myMap :: (x -> y) -> [x] -> [y]
-myMap f l = [f x | x <- l]
-
-main :: IO()
-main = do
-	putStrLn ":-B"
-	putStrLn $ show $ map sqrt [1, 2, 3, 4]
-	putStrLn $ show $ posicaoAlfabeto "abcxyz"
-	putStrLn $ show $ myMap sqrt [1, 2, 3, 4]
