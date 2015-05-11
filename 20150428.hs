3e774a569db296c73ee72953f33511bacef6c1e1
Miguel Araújo <miguelarauj1o@gmail.com>
2015-05-11 17:08:54 -0300

    AULA 08

--- 20150428.hs
+++ 20150428.hs
@@ -1,18 +1,1 @@
-module Main where
-
-(.).(.)
-
-(.) :: (u -> v) -> (t -> u) -> (t -> v)
-f == (.)
-
-(.) :: (b -> c) -> (a -> b) -> (a -> c)
-g == (.)
-
-(.).(.) :: f.g
-(u -> v) -> (t -> u) -> (t -> v)
-(b -> c) -> (v -> b) -> (v -> c)
-(t -> c)
-
-main :: IO()
-main = do
-	putStrLn "Hello World"
+-- AULA 08
