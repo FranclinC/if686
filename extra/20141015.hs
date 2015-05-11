5fc741ebb806e40a1d85844af9d94ff3d26e4309
Miguel Araújo <miguelarauj1o@gmail.com>
2015-05-11 14:08:25 -0300

    extra folder removed

--- extra/20141015.hs
+++
@@ -1,9 +0,0 @@
--- Lista 1, 2014.2
-module Main where
-
--- 8.
-buscaPalavra :: String -> [String] -> [String]
-
-main :: IO()
-main = do
-	putStrLn $ show $ buscaPalavra "pont" ["ponta", "apontar", "Ponteiro", "porto"]
