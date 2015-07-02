7f0ba9ac1062733f81c8f4530131bda808e75599
Miguel Araújo <miguelarauj1o@gmail.com>
2015-07-02 00:16:34 -0300

    aulas realocadas para pasta específica

---
+++ aulas/20150407.hs
@@ -0,0 +1,36 @@
+-- AULA 04
+
+type Horas = Int
+type Disciplinas = [String]
+
+data Dias = Segunda Horas Disciplinas
+	| Terca Horas Disciplinas
+	| Quarta Horas Disciplinas
+	| Quinta Horas Disciplinas
+	| Sexta Horas Disciplinas
+	| Sabado
+	| Domingo
+
+isFimDeSemana :: Dias -> Bool
+isFimDeSemana Sabado = True
+isFimDeSemana Domingo = True
+isFimDeSemana _ = False
+
+hasPLC :: Dias -> Bool
+hasPLC (Segunda h d) = elem "PLC" d
+hasPLC (Terca h d) = elem "PLC" d
+hasPLC (Quarta h d) = elem "PLC" d
+hasPLC (Quinta h d) = elem "PLC" d
+hasPLC (Sexta h d) = elem "PLC" d
+hasPLC Sabado = False
+hasPLC Domingo = False
+
+data List t = Nil | Cons t (List t)
+data Tree t = NilT | Node t (Tree t) (Tree t) deriving (Eq, Ord)
+
+--showExpr :: Expr -> String
+--fromList :: [t] -> List t
+--depth :: Tree t -> Int
+--collapse :: Tree t -> [t]
+--bfs :: Tree t -> t -> Bool
+--mapTree :: (t -> u) -> Tree t -> Tree u
