102cc7688b9fdce0fcc99e944bddaef0ab056538
Miguel Araújo <miguelarauj1o@gmail.com>
2015-07-04 11:20:45 -0300

    AULA 12

--- aulas/20150618.java
+++ aulas/20150618.java
@@ -22,8 +22,8 @@ public class Main implements Runnable {
 
         for (int i = 0; i < 10; i++) {
             randNum = rand.nextInt(20) + 1;
-            if(randNum % 2 == 0) { sq.add(randNum); System.out.println(randNum); }
-            else { tmp = sq.remove(); System.out.println(tmp); } 
+            if(randNum % 2 == 0) sq.add(randNum);
+            else tmp = sq.remove();
         }
     }
 
@@ -60,5 +60,82 @@ class SafeQueue {
     public synchronized int remove() {
         if(!v.isEmpty()) return v.remove(0);
         else return -999;
+    }
+}
+
+/*
+    Construa uma classe que implementa uma fila
+    segura para um número indeterminado de threads
+    que funcione da seguinte maneira.
+    - se apenas uma thread tentar inserir ou remover
+    um elemento, ela consegue
+    - se mais que uma estiver tentando ao mesmo tempo, uma
+    consegue e as outras esperam. A próxima só conseguirá
+    realizar a operação quando a anterior tiver terminado.
+    Porém, não é permitido usar blocos synchronized (e 
+    similares, como locks) para fazer esse exercício.
+*/
+
+import java.util.Random;
+import java.util.Vector;
+
+public class Main implements Runnable {
+
+    public void run() {
+
+        Random rand = new Random();
+        SafeQueue sq = new SafeQueue();
+        int randNum, tmp;
+
+        for (int i = 0; i < 10; i++) {
+            randNum = rand.nextInt(20) + 1;
+            if(randNum % 2 == 0) sq.add(randNum);
+            else tmp = sq.remove();
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        int N = Integer.parseInt(args[0]);
+        Thread[] t = new Thread[N];
+
+        for (int i = 0; i < N; i++) {
+            t[i] = (new Thread(new Main()));
+            t[i].start();
+        }
+
+        for (int i = 0; i < N; i++) {
+            try {
+                t[i].join();
+            }catch(InterruptedException e) {}
+        }
+    }
+}
+
+class SafeQueue {
+    
+    Vector<Integer> v;
+    static boolean monitor;
+
+    public SafeQueue() {
+        v = new Vector<Integer>();
+        this.monitor = true;
+    }
+
+    public void add(int i) {
+        while(monitor) {
+            monitor = false;
+            v.add(i);
+            monitor = true;
+        }
+    }
+
+    public int remove() {
+        while(monitor) {
+            monitor = false;
+            if(!v.isEmpty()) return v.remove(0);
+            else return -999;
+            monitor = true;
+        }
     }
 }
