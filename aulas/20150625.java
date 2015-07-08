0c48a30e22478504154d01142fc6ef2716bd31e4
Miguel Araújo <miguelarauj1o@gmail.com>
2015-07-08 17:41:10 -0300

    AULA 14

--- aulas/20150625.java
+++ aulas/20150625.java
@@ -101,3 +101,96 @@ class Node {
 		this.right = null;
 	}
 }
+
+/*
+	implemente um vetor (tamanho fixo) de
+	inteiros seguro para threads com operações
+	para ler e escrever em determinado índice e
+	para trocar os valores armazenados em dois
+	índices distintos.
+*/
+
+import java.util.Random;
+import java.util.concurrent.locks.*;
+
+public class Main implements Runnable {
+
+	static Var[] vector = new Var[30];
+
+	public void run() {
+
+		int x, p;
+		Random r = new Random();
+
+		for (int i = 0; i < 50; i++) {
+			
+			x = r.nextInt(1000) + 1;
+			p = (r.nextInt(1000) + 1) % 30;
+
+			if(i % 2 == 0) {
+				System.out.println("set: " + p + ", value: " + x);
+				set(p, x);
+			}else {
+				System.out.println("get: " + get(p));
+			} 
+		}
+	}
+
+	public void set(int p, int v) {
+		try {
+			vector[p].l.lock();
+			vector[p].value = v;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			vector[p].l.unlock();
+		}
+	}
+
+	public int get(int p) {
+
+		int var = 0;
+
+		try {
+			vector[p].l.lock();
+			var = vector[p].value;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			vector[p].l.unlock();
+			return var;
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		Thread[] t = new Thread[3];
+
+		for (int i = 0; i < 30; i++) {
+			vector[i] = new Var();
+			vector[i].l = new ReentrantLock();
+			vector[i].value = 0;
+		}
+
+		for (int i = 0; i < 3; i++) {
+			t[i] = (new Thread(new Main()));
+			t[i].start();
+		}
+
+		for (int i = 0; i < 3; i++) {
+			try {
+				t[i].join();
+			}catch(InterruptedException e) {}
+		}
+	}
+}
+
+class Var {
+	int value;
+	Lock l;
+
+	public Var() {
+		l = new ReentrantLock();
+	}
+}
+
